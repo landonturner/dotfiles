366279a123ac3a32ec78a987c53b94699d96d4f4
Landon Turner <landon.turner@getbraintree.com>
2017-03-22 11:36:41 -0500

    Enable automatic plugin installation

--- setup.sh
+++ setup.sh
@@ -2,9 +2,17 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Create symlinks
+mkdir ${DIR}/vim/colors
+
+# VIM installation
 ln -s ${DIR}/vim ~/.vim
 ln -s ${DIR}/vimrc ~/.vimrc
+
+vim +PlugInstall +qall
+
+ln -s ${DIR}/vim/plugged/vim-colors-solarized/colors/solarized.vim ${DIR}/vim/colors/solarized.vim
+
+# other symlinks
 ln -s ${DIR}/bashrc ~/.bashrc
 ln -s ${DIR}/tmux.conf ~/.tmux.conf
 
